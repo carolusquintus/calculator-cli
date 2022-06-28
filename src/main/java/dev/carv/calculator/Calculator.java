409a431926ae88829eae0b123bba7f7820c6b522
carolusquintus <carloschars@gmail.com>
2022-06-28 00:11:12 -0500

    First commit of calculator-cli sample based on Gradle for java-apps LinkedIn course

---
+++ src/main/java/dev/carv/calculator/Calculator.java
@@ -0,0 +1,49 @@
+package dev.carv.calculator;
+
+/**
+ * Basic implementation of a class Calculator.
+ */
+public class Calculator {
+
+    /**
+     * Adds two numbers.
+     *
+     * @param a first number to add.
+     * @param b second number to add.
+     * @return the sum of both numbers.
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtraction of two numbers.
+     * @param a first number to subtract.
+     * @param b second number to subtract.
+     * @return the subtraction of both numbers.
+     */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    /**
+     * Multiply two numbers.
+     * @param a first number to multiply.
+     * @param b second number to multiply.
+     * @return the multiplication of both numbers.
+     */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Division of two numbers.
+     *
+     * @param a first number to divide.
+     * @param b second number as dividend.
+     * @return the result of both division.
+     */
+    public double divide(int a, int b) {
+        return a / b;
+    }
+}
